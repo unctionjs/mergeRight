c076e38045dde53d6edbfb0487e7b01c265586b2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:44 -0700

    Autolinting

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 /* eslint-disable max-statements */
 import type from "@unction/type";
-import { merge } from "most";
-export default function mergeRight(left) {
-  return function mergeRightLeft(right) {
+import {merge} from "most";
+export default function mergeRight (left) {
+  return function mergeRightLeft (right) {
     if (type(left) !== type(right)) {
       throw new Error(`mergeRight received a ${type(left)} and ${type(right)} which aren't the same`);
     }
 
     switch (type(left)) {
       case "Array":
-        {
-          return [...left, ...right];
-        }
+      {
+        return [...left, ...right];
+      }
 
       case "Object":
-        {
-          return { ...left,
-            ...right
-          };
-        }
+      {
+        return {...left,
+          ...right,
+        };
+      }
 
       case "Map":
-        {
-          return new Map([...left, ...right]);
-        }
+      {
+        return new Map([...left, ...right]);
+      }
 
       case "Set":
-        {
-          return new Set([...left, ...right]);
-        }
+      {
+        return new Set([...left, ...right]);
+      }
 
       case "String":
-        {
-          return `${left}${right}`;
-        }
+      {
+        return `${left}${right}`;
+      }
 
       case "Stream":
-        {
-          return merge(left, right);
-        }
+      {
+        return merge(left, right);
+      }
 
       default:
-        {
-          throw new Error(`mergeRight doesn't know how to deal with ${type(left)}`);
-        }
+      {
+        throw new Error(`mergeRight doesn't know how to deal with ${type(left)}`);
+      }
     }
   };
 }
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 /* eslint-disable flowtype/require-variable-type, no-magic-numbers */
-import {test} from "tap"
-import {of} from "most"
-import streamSatisfies from "@unction/streamsatisfies"
+import {test} from "tap";
+import {of} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
 
-import mergeRight from "./index"
+import mergeRight from "./index";
 
 test("Array", ({same, end}) => {
   const left = [
     "a",
     "b",
-  ]
+  ];
   const right = [
     "c",
-  ]
+  ];
 
   same(
     mergeRight(left)(right),
@@ -21,21 +21,21 @@ test("Array", ({same, end}) => {
       "b",
       "c",
     ]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Object", ({same, end}) => {
   const left = {
     alpha: "2",
     beta: "1",
-  }
+  };
   const right = {
     alpha: "1",
     beta: "2",
     zeta: "3",
-  }
+  };
 
   same(
     mergeRight(left)(right),
@@ -44,50 +44,50 @@ test("Object", ({same, end}) => {
       beta: "2",
       zeta: "3",
     }
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Set", ({same, end}) => {
-  const left = new Set(["a", "1", "b", "0"])
-  const right = new Set(["b", "2", "c", "3"])
+  const left = new Set(["a", "1", "b", "0"]);
+  const right = new Set(["b", "2", "c", "3"]);
 
   same(
     mergeRight(left)(right),
     new Set(["a", "1", "b", "0", "2", "c", "3"])
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Map", ({same, end}) => {
-  const left = new Map([["a", "1"], ["b", "0"]])
-  const right = new Map([["b", "2"], ["c", "3"]])
+  const left = new Map([["a", "1"], ["b", "0"]]);
+  const right = new Map([["b", "2"], ["c", "3"]]);
 
   same(
     mergeRight(left)(right),
     new Map([["a", "1"], ["b", "2"], ["c", "3"]])
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("String", ({same, end}) => {
-  const left = "ab"
-  const right = "c"
+  const left = "ab";
+  const right = "c";
 
   same(
     mergeRight(left)(right),
     "abc"
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Stream", ({equal, doesNotThrow, end}) => {
-  const left = of("a")
-  const right = of("b")
+  const left = of("a");
+  const right = of("b");
 
   streamSatisfies(
     "'a'---'b'---|"
@@ -98,32 +98,32 @@ test("Stream", ({equal, doesNotThrow, end}) => {
   )(
     ({length}) =>
       (position) => {
-        equal(length, position)
-        end()
+        equal(length, position);
+        end();
       }
   )(
     mergeRight(left)(right)
-  )
-})
+  );
+});
 
 test(({throws, end}) => {
-  const left = {}
-  const right = []
+  const left = {};
+  const right = [];
 
   throws(
     () => mergeRight(left)(right)
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({throws, end}) => {
-  const left = 1
-  const right = 1
+  const left = 1;
+  const right = 1;
 
   throws(
     () => mergeRight(left)(right)
-  )
+  );
 
-  end()
-})
+  end();
+});
