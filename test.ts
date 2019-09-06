a485acb16250ab41ea7a06daa6dc50da7294f42b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 13:08:09 -0700

    Continuing to setup testing

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@
 import { of } from "most";
 import streamSatisfies from "@unction/streamsatisfies";
 
-import mergeRight from "./index.ts";
+import mergeRight from "./index";
 
 test("Array", () => {
   const left = [
