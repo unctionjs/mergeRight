9a8fed4bbd436f915ea0df5dd2a69987949cec40
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:50 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import type from "@unction/type";
 import {merge} from "most";
 
-export default function mergeRight<A> (left: Array<A> | Set<A> | RecordType<unknown, A> | string) {
-  return function mergeRightLeft (right: Array<A> | Set<A> | RecordType<unknown, A> | string) {
+export default function mergeRight<A> (left: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
+  return function mergeRightLeft (right: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
     if (type(left) !== type(right)) {
       throw new Error(`mergeRight received a ${type(left)} and ${type(right)} which aren't the same`);
     }
